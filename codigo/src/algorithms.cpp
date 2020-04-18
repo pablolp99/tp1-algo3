85c929bc6b78bae18f21d3003d561d6a9f11173d
Pablo <Lopes Perera>
2020-04-18 02:59:43 -0300

    project start

---
+++ codigo/src/algorithms.cpp
@@ -0,0 +1,5 @@
+#include "algorithms.h"
+
+int suma(int a , int b){
+	return a+b;
+}
---
+++ codigo/tests/algo_tests.cpp
@@ -0,0 +1,10 @@
+#include "gtest-1.8.1/gtest.h"
+#include "../src/algorithms.h"
+
+TEST(testing_algos, algo1){
+	ASSERT_TRUE(true);
+}
+
+TEST(testing_algos, algo2){
+	ASSERT_EQ(2, 2);
+}
