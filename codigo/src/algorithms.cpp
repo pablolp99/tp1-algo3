9aa57962948add6ef8453fea1ccbb08fab3bedec
Pablo <Lopes Perera>
2020-04-24 20:08:42 -0300

    ultimos cambios

--- codigo/src/algorithms.cpp
+++ codigo/src/algorithms.cpp
@@ -7,22 +7,36 @@ using namespace std;
 
 int brute_force(vector<array<int, 2>>& tape, int resistance, int cur_elem, vector<int> res_vect){
     if(cur_elem == tape.size()-1){
+        bool no_add_last = false;
+        bool was_broken = false;
         for(int i = 0; i < res_vect.size(); ++i){
+            if(res_vect[i]<0){
+                was_broken = true;
+            }
             res_vect[i]-=tape[cur_elem][WGH_INDEX];
             if(res_vect[i]<0){ // aca hay que ver si no se rompe el vector res_vect
-                return resistance >= 0 ? res_vect.size() : 0;
+                no_add_last = true;
             }
         }
-        return resistance - tape[cur_elem][WGH_INDEX] >= 0 ? res_vect.size() + 1 : 0;
+//        return resistance - tape[cur_elem][WGH_INDEX] >= 0 ? res_vect.size() + 1 : 0;
+        if(was_broken){
+            return 0;
+        }
+
+        if (resistance < 0){
+            return 0;
+        } else {
+            return no_add_last ? res_vect.size() : res_vect.size()+1;
+        }
     } else {
         int dont_add_elem = brute_force(tape, resistance, cur_elem+1, res_vect);
-
+        // Ahora dimos vuelta, primero hacemos el add, y despues hacemos en dont_add
         for(int i = 0; i < res_vect.size(); ++i){
             res_vect[i]-=tape[cur_elem][WGH_INDEX];
         }
         res_vect.push_back(tape[cur_elem][RES_INDEX]);
-
         int add_elem = brute_force(tape, resistance-tape[cur_elem][WGH_INDEX], cur_elem+1, res_vect);
+
         return max(add_elem, dont_add_elem);
     }
 }
@@ -47,8 +61,8 @@ int backtracking_factibilidad(vector<array<int, 2>>& tape, int resistance, int c
             }
         }
         res_vect.push_back(tape[cur_elem][RES_INDEX]);
-
         int add_elem = backtracking_factibilidad(tape, resistance-tape[cur_elem][WGH_INDEX], cur_elem+1, res_vect);
+
         return max(add_elem, dont_add_elem);
     }
 }
@@ -65,14 +79,12 @@ int backtracking_optimalidad(vector<array<int, 2>>& tape, int resistance, int cu
         }
         return resistance - tape[cur_elem][WGH_INDEX] >= 0 ? res_vect.size() + 1 : 0;
     } else {
+        int dont_add_elem = backtracking_factibilidad(tape, resistance, cur_elem+1, res_vect);
         // Optimalidad
         if(res_vect.size() + (tape.size()-cur_elem) <= MAX_SOLUTION){
             // calculo si agregando los 2 ult elementos superaria mi maximo
             return 0;
         }
-
-        int dont_add_elem = backtracking_factibilidad(tape, resistance, cur_elem+1, res_vect);
-
         for(int i = 0; i < res_vect.size(); ++i){
             res_vect[i]-=tape[cur_elem][WGH_INDEX];
             if(res_vect[i] < 0 || resistance < 0) {
@@ -82,8 +94,8 @@ int backtracking_optimalidad(vector<array<int, 2>>& tape, int resistance, int cu
 
         ++MAX_SOLUTION;
         res_vect.push_back(tape[cur_elem][RES_INDEX]);
-
         int add_elem = backtracking_factibilidad(tape, resistance-tape[cur_elem][WGH_INDEX], cur_elem+1, res_vect);
+
         return max(add_elem, dont_add_elem);
     }
 }
