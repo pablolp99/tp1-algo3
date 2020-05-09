6fed832cee030b16bcb7b39b390695286f405ec5
Pablo <Lopes Perera>
2020-05-08 22:35:35 -0300

    BOCA LA CONCHA DE TU MADRE

--- codigo/src/algorithms.cpp
+++ codigo/src/algorithms.cpp
@@ -63,15 +63,27 @@ int backtracking_optimalidad(vector<array<int, 2>>& tape, int resistance, int cu
 }
 
 int dynamic_programming_td(vector<array<int, 2>>& tape, int resistance) {
-    vector<vector<int>> m(tape.size() + 1, vector<int>(resistance + 1, -1));
-    return _dynamic_programming_td(tape, m, resistance, 1, 999999999);
+//    vector<vector<int>> m(tape.size() + 1, vector<int>(resistance + 1, -1));
+    vector<vector<int>> m(tape.size(), vector<int>(resistance+1, -1));
+//    int tmp = _dynamic_programming_td(tape, m, resistance, 0, resistance);
+    int tmp = _dynamic_programming_td_bis(tape, m, resistance, 0);
+    return tmp;
 }
 
-int _dynamic_programming_td(vector<array<int, 2>>& tape, vector<vector<int>>& m , int R, int cur_elem, int min_res){
+//int _dynamic_programming_td(vector<array<int, 2>>& tape, vector<vector<int>>& m , int R, int cur_elem, int min_res){
+//    if(R<0) return MENOS_INFINITO;
+//    if(min_res<0) return MENOS_INFINITO;
+//    if(cur_elem==tape.size()) return 0;
+//    if(m[cur_elem][R]==-1) m[cur_elem][R] = max(_dynamic_programming_td(tape, m, R, cur_elem+1, min_res),
+//                                               1+_dynamic_programming_td(tape, m, R-tape[cur_elem][WGH_INDEX], cur_elem+1, min(min_res-tape[cur_elem][WGH_INDEX], tape[cur_elem][RES_INDEX])));
+//    return m[cur_elem][R];
+//}
+
+
+int _dynamic_programming_td_bis(vector<array<int, 2>>& tape, vector<vector<int>>& m , int R, int cur_elem){
     if(R<0) return MENOS_INFINITO;
-    if(min_res<0) return MENOS_INFINITO;
-    if(cur_elem==tape.size()+1) return 0;
-    if(m[cur_elem][R]==-1) m[cur_elem][R] = max(_dynamic_programming_td(tape, m, R, cur_elem+1, min_res),
-                                               1+_dynamic_programming_td(tape, m, R-tape[cur_elem-1][WGH_INDEX], cur_elem+1, min(min_res-tape[cur_elem-1][WGH_INDEX], tape[cur_elem-1][RES_INDEX])));
+    if(cur_elem==tape.size()) return 0;
+    if(m[cur_elem][R]==-1) m[cur_elem][R] = max(_dynamic_programming_td_bis(tape, m, R, cur_elem+1),
+                                                1+_dynamic_programming_td_bis(tape, m, min(R-tape[cur_elem][WGH_INDEX], tape[cur_elem][RES_INDEX]), cur_elem+1));
     return m[cur_elem][R];
 }
