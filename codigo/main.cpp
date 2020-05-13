f8cc28f510014ad4f8fbf50f85e6b3271d25ef65
Pablo <Lopes Perera>
2020-05-13 20:35:12 -0300

    Nuevos graficos y datos

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -30,9 +30,9 @@ int main(){
 
         auto t1 = chrono::high_resolution_clock::now();
 //        int r = FB(tape, resistance, 0, 0);
-//        int r = BTF(tape, resistance, 0, 0);
+        int r = BTF(tape, resistance, 0, 0);
 //        int r = BTO_FLIPPED(tape, resistance, 0, 0);
-        int r = BTO(tape, resistance, 0, 0);
+//        int r = BTO(tape, resistance, 0, 0);
 //        int r = PD(tape, resistance);
         auto t2 = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count();
